aad964dc1151b91821233276849b26aea17697cb
Alex Warrington <awarrington0895@gmail.com>
2026-01-03 10:22:21 -0600

    feat: Add splitter to parse nodes with embedded code/italic/bold blocks
    CH3 L1

--- src/viperpress/generator.py
+++ src/viperpress/generator.py
@@ -2,6 +2,36 @@ from viperpress.textnode import TextNode, TextType
 import viperpress.htmlnode as html
 
 
+class MarkdownSyntaxError(Exception):
+    def __init__(self, node: TextNode):
+        self.node = node
+        super().__init__(f"Invalid markdown syntax: {node}")
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.Plain:
+            new_nodes.append(node)
+            continue
+
+        if node.text.count(delimiter) % 2 != 0:
+            raise MarkdownSyntaxError(node)
+
+        split = node.text.split(delimiter)
+
+        for i, part in enumerate(split):
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, TextType.Plain))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+
+    return new_nodes
+
+
 def text_node_to_html_node(text_node: TextNode):
     match text_node.text_type:
         case TextType.Plain:
--- src/viperpress/test_generator.py
+++ src/viperpress/test_generator.py
@@ -3,7 +3,7 @@ from viperpress.textnode import TextNode, TextType
 import viperpress.generator as generator
 
 
-class TestGenerator(unittest.TestCase):
+class TestTextToHtml(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.Plain)
         html_node = generator.text_node_to_html_node(node)
@@ -26,6 +26,63 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This link goes to google")
         self.assertEqual(html_node.props["href"], "https://www.google.com")
+
+
+class TestSplitter(unittest.TestCase):
+    def test_provided(self):
+        node = TextNode("This is text with a `code block` word", TextType.Plain)
+        new_nodes = generator.split_nodes_delimiter([node], "`", TextType.Code)
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.Plain),
+                TextNode("code block", TextType.Code),
+                TextNode(" word", TextType.Plain),
+            ],
+            new_nodes,
+        )
+
+    def test_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.Plain)
+        new_nodes = generator.split_nodes_delimiter([node], "**", TextType.Bold)
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.Plain),
+                TextNode("bolded", TextType.Bold),
+                TextNode(" word", TextType.Plain),
+            ],
+            new_nodes,
+        )
+
+    def test_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.Plain)
+        new_nodes = generator.split_nodes_delimiter([node], "_", TextType.Italic)
+        self.assertEqual(
+            [
+                TextNode("This is text with an ", TextType.Plain),
+                TextNode("italic", TextType.Italic),
+                TextNode(" word", TextType.Plain),
+            ],
+            new_nodes,
+        )
+
+    def test_multiple(self):
+        node = TextNode(
+            "This is text with `multiple code` blocks `within` it.  It `should` pose more of a challenge.",
+            TextType.Plain,
+        )
+        new_nodes = generator.split_nodes_delimiter([node], "`", TextType.Code)
+        self.assertEqual(
+            [
+                TextNode("This is text with ", TextType.Plain),
+                TextNode("multiple code", TextType.Code),
+                TextNode(" blocks ", TextType.Plain),
+                TextNode("within", TextType.Code),
+                TextNode(" it.  It ", TextType.Plain),
+                TextNode("should", TextType.Code),
+                TextNode(" pose more of a challenge.", TextType.Plain),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
